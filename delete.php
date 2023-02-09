5051fd23989f65ef4d7f17e4697689fe7dc47585
EmilianoRivasMX <emilianorivasxd@gmail.com>
2023-02-09 00:57:55 -0600

    Delete contact functionality

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php 
+
+require 'connection.php';
+
+$id = $_GET['id'];
+
+// Verifica si existe un contacto asociado con el id recibido
+$stmt = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$stmt->execute([":id" => $id]);
+
+if ($stmt->rowCount() == 0) {
+    http_response_code(404);
+    echo "HTTP 404 NOT FOUND";
+    return;
+}
+
+// Elimina el contacto
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
+
+header("Location: index.php");
